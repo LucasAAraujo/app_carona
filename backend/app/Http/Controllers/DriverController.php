3df8f2cd0bb0fc32e9300c5d24310ec69fdacaa4
Lucas Alves <lucasalves3.0@hotmail.com>
2024-11-01 10:12:57 -0300

    update: show e update com DriverController

---
+++ backend/app/Http/Controllers/DriverController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DriverController extends Controller
+{
+    public function show(Request $request)
+    {
+        // retornar o usuário e o driver model associado
+        $user = $request->user();
+        $user->load('driver');
+
+        return $request->user();
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'year' => 'required|numeric|between:2010,2024',
+            'make' => 'required',
+            'model' => 'required',
+            'color' => 'required|alpha',
+            'license_plate' => 'required',
+            'name' => 'required'
+        ]);
+
+        $user = $request->user();
+
+        $user->update($request->only('name'));
+
+        $user->driver()->updateOrCreate($request->only([
+            'year',
+            'make',
+            'model',
+            'color',
+            'license_plate'
+        ]));
+
+        $user->load('driver');
+
+        return $user;
+    }
+}
--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -21,13 +21,37 @@ class LoginController extends Controller
         ]);
 
         if(!$user) {
-            return response()->json(['message' => 'Nenhum usuário encontrado com este número.', 401]);
+            return response()->json(['message' => 'Nenhum usuário encontrado com este número.'], 401);
         }
         // enviar o usuário um ont-time user codigo
         $user->notify(new LoginNeedsVerification());
 
         // retornar a resposta
         return response()->json(['message' => 'SMS enviado.']);
+    }
 
+    public function verify(Request $request)
+    {
+        // validaçao da requisição
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+
+        // achar o usuário
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+        // O código providenciado é o mesmo salvo?
+        // Se sim, return back com auth token
+        if ($user) {
+            $user->update([
+                'login_code' => null
+            ]);
+
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
+        //Se não, return back com uma menssagem
+        return response()->json(['message' => 'Código de verificação inválido.'], 401);
     }
 }
